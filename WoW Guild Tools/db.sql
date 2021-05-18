49c9957fe277898dcfa434a00435f008eb922614
Blizz991 <martinhblizz@gmail.com>
2021-05-18 22:07:14 +0200

    Added db scripts, removed database from deploy (did it manually)

---
+++ WoW Guild Tools/db.sql
@@ -0,0 +1,169 @@
+Build started...
+Build succeeded.
+CREATE TABLE [AspNetRoles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [AspNetUsers] (
+    [Id] nvarchar(450) NOT NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Raiders] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(12) NOT NULL,
+    [Race] nchar(8) NOT NULL,
+    [Class] nchar(8) NOT NULL,
+    [Spec] nchar(13) NOT NULL,
+    [Role] nchar(6) NOT NULL,
+    [Rank] nchar(11) NOT NULL,
+    [Profession1] nchar(15) NOT NULL,
+    [Profession2] nchar(15) NOT NULL,
+    [MainId] int NULL,
+    CONSTRAINT [PK_Raiders] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Raiders_Raiders_MainId] FOREIGN KEY ([MainId]) REFERENCES [Raiders] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+
+CREATE TABLE [RaidGroups] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+    [Published] bit NOT NULL,
+    [RaidDay] datetime2 NOT NULL,
+    [LastUpdated] datetime2 NOT NULL DEFAULT (getdate()),
+    CONSTRAINT [PK_RaidGroups] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] nvarchar(128) NOT NULL,
+    [ProviderKey] nvarchar(128) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(128) NOT NULL,
+    [Name] nvarchar(128) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE TABLE [GroupRaiders] (
+    [Id] int NOT NULL IDENTITY,
+    [GroupNumber] int NOT NULL,
+    [GroupPosition] int NOT NULL,
+    [RaidGroupId] int NOT NULL,
+    [RaiderId] int NOT NULL,
+    CONSTRAINT [PK_GroupRaiders] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_GroupRaiders_Raiders_RaiderId] FOREIGN KEY ([RaiderId]) REFERENCES [Raiders] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_GroupRaiders_RaidGroups_RaidGroupId] FOREIGN KEY ([RaidGroupId]) REFERENCES [RaidGroups] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+GO
+
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+GO
+
+
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+GO
+
+
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+GO
+
+
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+GO
+
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+GO
+
+
+CREATE INDEX [IX_GroupRaiders_RaiderId] ON [GroupRaiders] ([RaiderId]);
+GO
+
+
+CREATE INDEX [IX_GroupRaiders_RaidGroupId] ON [GroupRaiders] ([RaidGroupId]);
+GO
+
+
+CREATE INDEX [IX_Raiders_MainId] ON [Raiders] ([MainId]);
+GO
+
+
+
