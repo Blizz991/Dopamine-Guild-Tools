f912e6b7f18ef6ca5f80524522bb48aca294692a
Blizz991 <martinhblizz@gmail.com>
2022-08-19 04:09:43 +0200

    Initial setup for tracking draenei buffs across raid groups

---
+++ WoW Guild Tools/Models/RaidBuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WoW_Guild_Tools.Models.Enums;
+
+namespace WoW_Guild_Tools.Models
+{
+    public class RaidBuff
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int SpellId { get; set; }
+        public WowClass Class { get; set; }
+        public WowSpec Spec { get; set; }
+    }
+}
